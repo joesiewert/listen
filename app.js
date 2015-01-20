61358f2aeec7c7ca573de48547b22b85898020ff
joesiewert <joe.siewert@gmail.com>
2015-01-19 23:02:53 -0700

    Setup play/pause buttons

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+'use strict';
+
 $.getJSON('data.json', function(tracks) {
   var $playerTemplate = $('#playerTemplate').html();
   var playerHTML = Mustache.to_html($playerTemplate, tracks);
   $('#player').html(playerHTML);
+})
+
+$('.container').on('click', '.play', function() {
+  $('.icon').removeClass('pause').addClass('play');
+  $(this).removeClass('play').addClass('pause');
+})
+
+$('.container').on('click', '.pause', function() {
+  $(this).removeClass('pause').addClass('play');
 })
