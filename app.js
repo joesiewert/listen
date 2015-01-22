9eb22f1148d004c413c3ae59daaa60d23f367987
joesiewert <joe.siewert@gmail.com>
2015-01-21 22:48:17 -0700

    Switched ids to data attributes

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 'use strict';
 
 $.getJSON('data.json', function(tracks) {
-  var $playerTemplate = $('#playerTemplate').html();
+  var $playerTemplate = $("[data-template='playerTemplate']").html();
   var playerHTML = Mustache.to_html($playerTemplate, tracks);
-  $('#player').html(playerHTML);
-})
+  $("[data-body='player']").html(playerHTML);
+});
 
-$('.container').on('click', '.play', function() {
+$("[data-body='player']").on('click', '.play', function() {
   $('.icon').removeClass('pause').addClass('play');
   $(this).removeClass('play').addClass('pause');
-})
+});
 
-$('.container').on('click', '.pause', function() {
+$("[data-body='player']").on('click', '.pause', function() {
   $(this).removeClass('pause').addClass('play');
-})
+});
