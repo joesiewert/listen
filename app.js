df5c0d05c68e8b836e052457c6e7e06f54797396
joesiewert <joe.siewert@gmail.com>
2015-01-22 11:50:25 -0700

    Tracks now play/stop

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 'use strict';
+$(document).ready(function() {
 
-$.getJSON('data.json', function(tracks) {
-  var $playerTemplate = $("[data-template='playerTemplate']").html();
-  var playerHTML = Mustache.to_html($playerTemplate, tracks);
-  $("[data-body='player']").html(playerHTML);
-});
+  $.getJSON('data.json', function(tracks) {
+    var $playerTemplate = $("[data-template='playerTemplate']").html();
+    var playerHTML = Mustache.to_html($playerTemplate, tracks);
+    $("[data-body='player']").html(playerHTML);
+  });
 
-$("[data-body='player']").on('click', '.play', function() {
-  $('.icon').removeClass('pause').addClass('play');
-  $(this).removeClass('play').addClass('pause');
-});
+  $("[data-body='player']").on('click', '.play', function() {
+    $('.icon').removeClass('pause').addClass('play');
+    $(this).removeClass('play').addClass('pause');
 
-$("[data-body='player']").on('click', '.pause', function() {
-  $(this).removeClass('pause').addClass('play');
+    $('audio').each(function(i, element) {
+      element.pause();
+    });
+
+    var id = $(this).data('id');
+    var track = document.getElementsByTagName('audio')[id];
+    track.load();
+    track.play();
+  });
+
+  $("[data-body='player']").on('click', '.pause', function() {
+    $(this).removeClass('pause').addClass('play');
+
+    var id = $(this).data('id');
+    var track = document.getElementsByTagName('audio')[id];
+    track.pause();
+  });
+
 });
